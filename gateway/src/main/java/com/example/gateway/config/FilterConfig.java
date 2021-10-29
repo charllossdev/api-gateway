b4ec1aed34394fd5f3198f589c8753542c297b62
charlloss.dev <charlloss.dev@gmail.com>
2021-10-30 05:59:45 +0900

    create: init spring cloud gateway

---
+++ gateway/src/main/java/com/example/gateway/config/FilterConfig.java
@@ -0,0 +1,24 @@
+package com.example.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/service-admin/**")
+                        .filters(f -> f.addRequestHeader("service-request", "service-admin-header")
+                                .addResponseHeader("service-response", "service-admin-response"))
+                        .uri("http://localhost:8001"))
+                .route(r -> r.path("/service-user/**")
+                        .filters(f -> f.addRequestHeader("service-request", "service-user-header")
+                                .addResponseHeader("service-response", "service-user-response"))
+                        .uri("http://localhost:9001"))
+                .build();
+    }
+}
---
+++ service-admin/src/main/java/com/example/serviceadmin/api/ApiController.java
@@ -0,0 +1,25 @@
+package com.example.serviceadmin.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service-admin")
+@Slf4j
+public class ApiController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome to the admin service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("service-request")String header) {
+        log.info(header);
+        return header;
+    }
+}
